e73b81acfdd1ac181a164207235450ed14d9a6a0
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-06-15 08:20:37 +0300

    Git update

---
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -0,0 +1,7 @@
+import { Button } from "../ui/button"
+
+export default function WelcomeSection() {
+    return (
+        <Button className='bg-gradient-to-r from-cyan-500 to-fuchsia-500 hover:from-fuchsia-500 hover:to-violet-500'>CLick me</Button>
+    )
+}
