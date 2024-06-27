65b17f5560a969af1a5d06718c9ebdb430ba6039
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-06-27 16:39:13 +0300

    Update: navbar

--- src/components/Experimental/PillNavbarFollowCursor.tsx
+++ src/components/Experimental/PillNavbarFollowCursor.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import { motion, AnimatePresence, useScroll } from 'framer-motion';
 import { useRef, useState } from "react";
-
+import { ReactLenis, useLenis } from 'lenis/react';
 
 // source https://www.youtube.com/watch?v=obib4ka06y0
 
@@ -12,7 +12,13 @@ interface PositionProps {
 }
 
 export default function PillNavBarComponent() {
+    const [state, setState] = useState<number>(0);
     const ref = useRef(null);
+    const lenis = useLenis(({ scroll }) => {
+        // called every scroll
+        // console.log(scroll)
+        setState(scroll)
+      }, )
 
     const [position, setPosition] = useState<PositionProps>({
         left: 220,
@@ -27,13 +33,19 @@ export default function PillNavBarComponent() {
         }))
     }
 
-    const navStyles: string = 'fixed top-8 left-[38vw] z-50 backdrop-blur flex w-auto gap-2 border-transparent hover:border-transparent h-full border-2 h-15 rounded-lg justify-evenly p-1 max-w-[660px]';
+    const navStyles: string = 'fixed z-50 backdrop-blur-sm bg-slate-200/50 rounded-b-2xl flex w-auto gap-2 h-full h-15 w-full justify-evenly p-1 max-w-[660px]';
     // TSX component
 
     return (
-        <nav 
+        <AnimatePresence mode="wait">
+        {state < 1400  ?         
+        <motion.nav 
         ref={ref}
         onMouseLeave={resetPos}
+        initial={{x: 0, opacity: 0}}
+        animate={{x: 0, opacity: 1}}
+        exit={{x: 0, opacity: 0}}
+        transition={{duration: 0.4}}
         className={cn(navStyles)}>
             <LinkItem setPosition={setPosition} href='/'>Home</LinkItem>
             <LinkItem setPosition={setPosition} href='/about'>About</LinkItem>
@@ -41,13 +53,14 @@ export default function PillNavBarComponent() {
             <LinkItem setPosition={setPosition} href='/blog'>Blog</LinkItem>
             {/* <LinkItem setPosition={setPosition} href={`/${path}`}>{path}</LinkItem> */}
             <CursorBackground position={position} />
-        </nav>
+        </motion.nav> : null}
+        </AnimatePresence>
     )
 }
 
 const LinkItem = ({ children, href, setPosition } : any) => {
     const ref = useRef<HTMLAnchorElement>(null);
-    const aStyles: string = 'flex z-40 h-12 w-auto bg-transparent items-center justify-center px-5 py-2 rounded-lg mix-blend-multiply';
+    const aStyles: string = 'flex z-40 h-12 w-auto bg-transparent items-center justify-center px-2 md:px-5 py-2 rounded-lg mix-blend-multiply';
     const aTextStyles: string = 'text-zinc-800 font-semibold uppercase'
 
     const movePos = () : void => {
