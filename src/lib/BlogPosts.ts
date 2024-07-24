af41cc84c87657063801526fcee78b93cea58745
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-07-24 12:20:02 +0300

    Misc: blog posts loaded from file (not from state)

---
+++ src/lib/BlogPosts.ts
@@ -0,0 +1,71 @@
+interface BlogPostProps {
+  title: string;
+  subtitle?: string;
+  content: string;
+  dateCreated: string;
+  topic: string;
+  primary?: boolean;
+  secondary?: boolean;
+  tertiary?: boolean;
+  sourceUrl?: string;
+}
+
+const blogPosts: BlogPostProps[] = [
+  {
+    title: "What is new in JS?",
+    subtitle: "The new ES2024 is approved",
+    content:
+      "There is officially a new version of the language JavaScript. You may have seen most of these features already but they are now officially approved and standardized.",
+    dateCreated: "24/07/2024",
+    topic: "conference news",
+    primary: true,
+    sourceUrl: "https://www.youtube.com/watch?v=tKHdASf_OOI",
+  },
+  {
+    title: "Unicode-related utilities",
+    content:
+      "Assert if strings are valid before searching the database and avoid invalid encoding errors. Use the new .isWellFormed property to test the validity of an emoji or use v.test(emoji) for regex.",
+    dateCreated: "24/07/2024",
+    topic: "JS",
+    secondary: true,
+  },
+  {
+    title: "Raw memory and multithreading",
+    subtitle: "Atomics.waitAsync",
+    content:
+      "Resizable and growable (Shared)ArrayBuffers and ArrayBuffer transfer",
+    dateCreated: "24/07/2024",
+    topic: "JS",
+    secondary: true,
+  },
+  {
+    title: "Promise.withResolvers",
+    subtitle: "Convenience method",
+    content:
+      "New way of creating promises that might be useful when working with some non-promise based APIs.",
+    dateCreated: "24/07/2024",
+    topic: "tech",
+    secondary: true,
+  },
+  {
+    title: "Object.groupBy",
+    subtitle: "Convenience method",
+    content:
+      "Created a new object where each key represents each key property and its value is an array of all objects with that property.",
+    dateCreated: "24/07/2024",
+    topic: "JS",
+    tertiary: true,
+  },
+  {
+    title: "Working with sets",
+    content:
+      "Set objects now get all methods that other languages provide. Check out .intersection(), .union() and the docs.",
+    dateCreated: "24/07/2024",
+    topic: "math",
+    tertiary: true,
+  },
+
+];
+
+export { blogPosts };
+export type { BlogPostProps };
