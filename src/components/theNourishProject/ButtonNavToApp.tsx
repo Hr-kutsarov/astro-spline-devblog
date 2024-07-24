f736cd972e3dc5389d82d2e3f467c15438e422b7
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-07-24 12:22:27 +0300

    Fix: Nourish page-> navigate to App from Hero button styles fix

--- src/components/theNourishProject/ButtonNavToApp.tsx
+++ src/components/theNourishProject/ButtonNavToApp.tsx
@@ -16,22 +16,24 @@ const NavToAppButton = () => {
 
     const btnStyles: string = "h-12 text-lg font-semibold  rounded-lg "
     const secondaryColors: string = "hover:bg-gradient-to-br from-zinc-100 to-slate-200";
-    const primaryColors: string = 'text-zinc-50 bg-gradient-to-br from-green-500 to-emerald-500'
+    const primaryColors: string = 'text-zinc-50 bg-gradient-to-br from-green-500 to-emerald-500';
+    const responsiveButtons: string = "min-w-24"
+    
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
         <Button className={cn("h-12 text-sm font-bold text-slate-700 bg-slate-100 mt-8 w-40 hover:no-underline")} variant="link">To App</Button>
       </AlertDialogTrigger>
       <AlertDialogContent className="rounded-lg">
-        <AlertDialogHeader className='my-12'>
+        <AlertDialogHeader className='my-12 md:my-4 md:px-4'>
           <AlertDialogTitle className='text-3xl'>Are you sure?</AlertDialogTitle>
           <AlertDialogDescription className="text-lg">
             You will be redirected to the Application.
           </AlertDialogDescription>
         </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel className={cn(btnStyles,secondaryColors)}>Cancel</AlertDialogCancel>
-          <AlertDialogAction className={cn(btnStyles, primaryColors)}>
+        <AlertDialogFooter className='md:w-full  md:px-4'>
+          <AlertDialogCancel className={cn(btnStyles,secondaryColors, responsiveButtons)}>Cancel</AlertDialogCancel>
+          <AlertDialogAction className={cn(btnStyles, primaryColors, responsiveButtons)}>
             <a href="/">
             Continue
             </a>
